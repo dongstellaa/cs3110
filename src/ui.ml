f5446438f2cf00cab561cb1a199f435e235fd33c
ssd74 <ssd74@cornell.edu>
2023-03-18 18:27:51 -0400

    implement ui.ml
    - work on output from grid to printing row

--- src/ui.ml
+++ src/ui.ml
@@ -1,7 +1,21 @@
-let top_line = "┌─────┬─────┬─────┬─────┐"
+let top_line = "┌───────┬───────┬───────┬───────┐"
 
-let middle_line = "├─────┼─────┼─────┼─────┤"
+let middle_line = "├───────┼───────┼───────┼───────┤"
 
-let bottom_line = "└─────┴─────┴─────┴─────┘"
+let bottom_line = "└───────┴───────┴───────┴───────┘"
 
-let col = "│"
+let col = "│" 
+
+let cell value = (String.make (6 - (String.length value)) ' ') ^ value
+
+let build_row row=
+  let elem_strings = List.map (fun elem -> Printf.sprintf "│ %6d " elem) row in
+  String.concat "" elem_strings ^ "│" 
+(* 
+let build_grid grid = List.map (fun row ->) *)
+
+let output = 
+  print_endline top_line
+
+
+
