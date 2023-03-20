51dbfe91f06e7d40dd359dff5251c574bfbcb594
ssd74 <ssd74@cornell.edu>
2023-03-20 10:13:41 -0400

    fail with incorrect key
    - if user inputs a key that isn't an option, fails with "incorrect key" instead of "not implemented"

--- src/input.ml
+++ src/input.ml
@@ -9,10 +9,8 @@ let key_to_action = function
   | "d" -> Move Right
   | _ -> Invalid
 
-(* let input_to_action () = read_line () |> key_to_action *)
-
 let grid_action input input_grid =
   let a = key_to_action input in
   match a with
   | Move m -> move_grid m input_grid
-  | Invalid -> failwith "not implemented"
+  | Invalid -> failwith "Incorrect key"
