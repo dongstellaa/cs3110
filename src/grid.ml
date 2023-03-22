cdc5960ec0466e191e5d04ef29e8cafa1045aa21
edf55 <edf55@cornell.edu>
2023-03-22 18:35:52 -0400

    edited grid.ml to fix addition issues
    -changed and created test cases and grids to ensure addition is correct

--- src/grid.ml
+++ src/grid.ml
@@ -3,14 +3,13 @@ type grid = string list list
 let board : grid = [ [ "" ] ]
 
 let finish_multiplier_tail row =
-  List.rev
-    (let rec shift_helper acc = function
-       | [] -> acc
-       | h :: tl when h = 0 -> shift_helper acc tl
-       | h :: tl when acc = [] -> shift_helper [ h ] tl
-       | h :: tl -> shift_helper (h :: acc) tl
-     in
-     shift_helper [] row)
+  let rec shift_helper acc = function
+    | [] -> acc
+    | h :: tl when h = 0 -> shift_helper acc tl
+    | h :: tl when acc = [] -> shift_helper [ h ] tl
+    | h :: tl -> shift_helper (h :: acc) tl
+  in
+  shift_helper [] row
 
 let delete_zeros row =
   let rec shift_helper acc = function
@@ -23,23 +22,30 @@ let delete_zeros row =
   in
   shift_helper [] row
 
+(* let left_shift row =
+   match List.length (delete_zeros row) with
+   | 0 -> List.rev (delete_zeros row) @ [ 0; 0; 0; 0 ]
+   | 1 -> List.rev (delete_zeros row) @ [ 0; 0; 0 ]
+   | 2 -> List.rev (delete_zeros row) @ [ 0; 0 ]
+   | 3 -> List.rev (delete_zeros row) @ [ 0 ]
+   | _ -> List.rev (delete_zeros row) @ [] *)
+
 let left_shift row =
   match List.length (delete_zeros row) with
-  | 0 -> List.rev (delete_zeros row) @ [ 0; 0; 0; 0 ]
-  | 1 -> List.rev (delete_zeros row) @ [ 0; 0; 0 ]
-  | 2 -> List.rev (delete_zeros row) @ [ 0; 0 ]
-  | 3 -> List.rev (delete_zeros row) @ [ 0 ]
-  | _ -> List.rev (delete_zeros row) @ []
-
+  | 0 -> delete_zeros row @ [ 0; 0; 0; 0 ]
+  | 1 -> delete_zeros row @ [ 0; 0; 0 ]
+  | 2 -> delete_zeros row @ [ 0; 0 ]
+  | 3 -> delete_zeros row @ [ 0 ]
+  | _ -> delete_zeros row @ []
 (* let right_shift row = List.rev row |> shift_left |> List.rev *)
 
 let right_shift row =
   match List.length (delete_zeros row) with
-  | 0 -> [ 0; 0; 0; 0 ] @ List.rev (delete_zeros row)
-  | 1 -> [ 0; 0; 0 ] @ List.rev (delete_zeros row)
-  | 2 -> [ 0; 0 ] @ List.rev (delete_zeros row)
-  | 3 -> [ 0 ] @ List.rev (delete_zeros row)
-  | _ -> List.rev (delete_zeros row)
+  | 0 -> [ 0; 0; 0; 0 ] @ delete_zeros row
+  | 1 -> [ 0; 0; 0 ] @ delete_zeros row
+  | 2 -> [ 0; 0 ] @ delete_zeros row
+  | 3 -> [ 0 ] @ delete_zeros row
+  | _ -> delete_zeros row
 
 let left_shift_grid grid = List.map left_shift grid
 let right_shift_grid grid = List.map right_shift grid
--- src/main.ml
+++ src/main.ml
@@ -1,26 +1,33 @@
-(* let test_grid_1 =
+(* let test_grid =
    [ [ 2; 0; 2; 0 ]; [ 4; 0; 0; 0 ]; [ 0; 0; 0; 0 ]; [ 2; 0; 2; 2 ] ] *)
 
-let test_grid_2 =
-  [ [ 0; 0; 4; 8 ]; [ 4; 2; 0; 8 ]; [ 0; 2; 0; 0 ]; [ 16; 0; 4; 8 ] ]
+(* let test_grid =
+   [ [ 0; 0; 4; 8 ]; [ 4; 2; 0; 8 ]; [ 0; 2; 0; 0 ]; [ 16; 0; 4; 8 ] ] *)
 
-(* let test_grid_3 =
-   [[1024; 1024; 0; 0];
-   [0; 0; 0; 0];
-   [0; 0; 0; 0];
-   [0; 0; 0; 0]] *)
+(* let test_grid =
+   [ [ 1024; 1024; 0; 0 ]; [ 0; 0; 0; 0 ]; [ 0; 0; 0; 0 ]; [ 0; 0; 0; 0 ] ] *)
+
+let test_grid =
+  [ [ 2; 2; 0; 0 ]; [ 2; 2; 0; 4 ]; [ 2; 2; 4; 8 ]; [ 0; 8; 8; 16 ] ]
+
+(* let test_grid =
+   [ [ 2; 2; 2; 2 ]; [ 2; 2; 2; 2 ]; [ 2; 2; 2; 2 ]; [ 2; 2; 2; 2 ] ] *)
+
+(* let test_grid =
+   [ [ 2; 2; 4; 8 ]; [ 4; 4; 8; 8 ]; [ 16; 64; 32; 32 ]; [ 16; 8; 4; 2 ] ] *)
 
 let user_input grid () =
-  print_endline "Press w, a, s, d to play or quit to exit:";
+  print_endline "Press w, a, s, d to play or q to exit:";
   Ui.output grid;
   let rec user_input_helper grid =
     let input = read_line () in
-    if input = "quit" then print_endline "quitting 2048"
+    if input = "q" then print_endline "quitting 2048"
     else
       let new_grid = Input.grid_action input grid in
       Ui.output new_grid;
+      print_endline "Press w, a, s, d to play or q to exit:";
       user_input_helper new_grid
   in
   user_input_helper grid
 
-let () = user_input test_grid_2 ()
+let () = user_input test_grid ()
